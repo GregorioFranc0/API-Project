a90359a038fbce4c6ed1247a7d9c86d50cacf3ca
gregorio <gregorio.franco1985@gmail.com>
2024-02-22 22:25:47 -0500

    added options to migrations

--- backend/db/migrations/20240222020729-create-spot.js
+++ backend/db/migrations/20240222020729-create-spot.js
@@ -1,5 +1,9 @@
 'use strict';
-
+let options = {};
+const { sequelize } = require('../models');
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
@@ -59,7 +63,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
