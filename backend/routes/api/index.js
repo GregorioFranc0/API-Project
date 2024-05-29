c081fe5d4e870242793db798a00b767ea9a9b054
gregorio <gregorio.franco1985@gmail.com>
2024-05-28 20:25:59 -0400

    editing spots routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,6 +11,9 @@ const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 const spot = require('../../db/models/spot.js');
 
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
@@ -34,7 +37,15 @@ router.get(
     }
 );
 
-
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+});
 
 router.get(
     '/require-auth',
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,34 +10,83 @@ const router = express.Router();
 
 //get all spots
 
-router.get(async (req, res, next) => {
-    const spot = await Spot.findAll();
+router.get(
+    '/',
+    async (req, res) => {
+        let { page, size } = req.query;
 
-    const validSpot = {
-        "id": spot.id,
-        "ownerId": spot.ownerId,
-        "address": spot.address,
-        "city": spot.city,
-        "state": spot.state,
-        "country": spot.country,
-        "lat": spot.lat,
-        "lng": spot.lng,
-        "description": spot.description,
-        "price": spot.price,
-        "createdAt": spot.createdAt,
-        "updatedAt": spot.updatedAt,
-        "avgRating": spot.avgRating,
-        "previewImage": spot.previewImage
-    };
+        page = Number(page);
+        size = Number(size);
 
+        if (Number.isNaN(page)) page = 1;
+        if (Number.isNaN(size)) size = 20
 
+        if (page <= 0) {
+            return res.status(400).json({
+                "message": "Validation Error",
+                "statusCode": 400,
+                "errors": {
+                    "page": "Page must be greater than or equal to 1"
+                }
+            })
+        }
+        if (size <= 0) {
 
-    return res.json({
-        spot: validSpot
-    });
-})
+            return res.status(400).json({
+                "message": "Validation Error",
+                "statusCode": 400,
+                "errors": {
+                    "size": "Size must be greater than or equal to 1"
+                }
+            })
+        }
 
-// Get all spots owned by the current user
+        const spots = await Spot.findAll({
+            limit: size,
+            offset: size * (page - 1)
+        })
+
+        for await (let spot of spots) {
+            const previewImage = await Image.findOne({
+                where: {
+                    imageId: spot.id,
+                    preview: true,
+                    imageType: "Spot"
+                }
+            });
+            if (previewImage && !spot.previewImage) {
+                spot.previewImage = previewImage.url
+            } else if (!previewImage && !spot.previewImage) {
+                spot.previewImage = "None"
+            } else {
+                spot.previewImage = spot.previewImage
+            }
+
+            const rating = await Review.findAll({
+                where: { spotId: spot.id }
+            })
+
+            let sum = 0;
+
+            if (rating.length) {
+                rating.forEach(rating => {
+                    sum += rating.stars
+                });
+                let avg = sum / rating.length;
+
+                spot.avgRating = avg
+            } else {
+                spot.avgRating = 0
+            }
+        }
+
+        res.json({
+            Spots: spots, page, size
+        })
+    }
+)
+
+// Get all spots owned by current user
 router.get(
     '/current',
     requireAuth,
@@ -92,5 +141,73 @@ router.get(
         res.json({ Spots: spots })
     }
 )
+
+// Get details of spot from ID
+router.get(
+    '/:spotId',
+    async (req, res) => {
+
+        const spot = await Spot.findByPk(req.params.spotId, {
+
+        });
+
+        if (spot) {
+            const previewImage = await Image.findOne({
+                where: {
+                    imageId: req.params.spotId,
+                    preview: true,
+                    imageType: "Spot"
+                }
+            });
+
+
+            spot.dataValues.SpotImages = []
+            const allImages = await Image.findAll({
+                where: {
+                    imageId: req.params.spotId,
+                    imageType: "Spot"
+                }
+            })
+            spot.dataValues.SpotImages = [...allImages]
+
+
+            if (previewImage && !spot.previewImage) {
+                spot.previewImage = previewImage.url
+                // spot.dataValues.SpotImages.push(previewImage.url)
+            } else if (!previewImage && !spot.previewImage) {
+                spot.previewImage = "None"
+            } else {
+                spot.previewImage = spot.previewImage
+            }
+
+            const rating = await Review.findAll({
+                where: { spotId: req.params.spotId }
+            })
+
+            let sum = 0;
+
+            if (rating.length) {
+                rating.forEach(rating => {
+                    sum += rating.stars
+                });
+                let avg = sum / rating.length;
+
+                spot.avgRating = avg
+            } else {
+                spot.avgRating = 0
+            }
+        }
+
+        if (spot.id === null) {
+
+            res.status(404);
+            return res.json({
+                message: "Spot couldn't be found",
+                stateCode: 404
+            })
+        }
+        res.json(spot);
+    }
+);
 
 module.exports = router;
